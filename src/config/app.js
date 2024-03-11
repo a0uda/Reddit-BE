013d1da9b739a81a3a40df218c5913bc1bef38bd
Malakyasser8 <malakyasser8@gmail.com>
2024-03-12 01:49:52 +0200

    Connected to database

---
+++ src/config/app.js
@@ -0,0 +1,5 @@
+const mongoose = require("mongoose");
+
+mongoose.connect(
+  "mongodb+srv://ahmedabdelgawad011:BackendReddit@cluster0.workift.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+);
