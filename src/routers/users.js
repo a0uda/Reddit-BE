b98b1078623339446dbeb642dbfe6b6808f10bc8
Ahmed <ahmed.abdelgawad011@eng-st.cu.edu.eg>
2024-03-14 01:26:02 +0200

    Login/out done

--- src/routers/users.js
+++ src/routers/users.js
@@ -1,45 +1,63 @@
 import express from "express";
-import {User} from "../db/models/User.js"; //if error put .js
-import { addNewUser, isUsernameAvailable } from "../utils/users.js";
+import { User } from "../db/models/User.js"; //if error put .js
+import { signupUser, loginUser, logoutUser } from "../utils/userAuth.js";
 
 export const usersRouter = express.Router();
 
+usersRouter.post("/users/signup", async (req, res) => {
+  try {
+    const { success, err, user } = await signupUser(req.body);
 
-usersRouter.post("/users/signup",async (req,res) => {
-    try {
-    const { success, err, user } = await addNewUser(req.body);
-
-    if (!success){
-        res.status(400).send(err);
-        return;
+    if (!success) {
+      res.status(400).send(err);
+      return;
     }
 
-    res.status(201).send( {username:user.username, access_token:user.token});
+    res.header("Authorization", `Bearer ${user.token}`);
 
-    } catch (error) {
-
+    res.status(201).send({ username: user.username });
+  } catch (error) {
     res.status(500).json({ error: "Internal server error." });
-}
-    return;
-})
+  }
+  return;
+});
 
+usersRouter.post("/users/login", async (req, res) => {
+  try {
+    const { success, err, user } = await loginUser(req.body);
 
+    if (!success) {
+      res.status(400).send(err);
+      return;
+    }
 
+    // Set the token in the response header
+    res.header("Authorization", `Bearer ${user.token}`);
 
+    // Send a response without the token in the body
+    res.status(200).send({ username: user.username });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Internal server error." });
+  }
+  return;
+});
 
+usersRouter.post("/users/logout", async (req, res) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    const { username } = req.body;
 
+    const { success, msg, err } = await logoutUser({ token, username });
 
+    if (!success) {
+      res.status(400).send(err);
+      return;
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+    res.status(200).send(msg);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Internal server error." });
+  }
+});
