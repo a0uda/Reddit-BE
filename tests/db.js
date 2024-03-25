1fcc61fef083006aa0916a6f4838492c12e4da4c
Malakyasser8 <malakyasser8@gmail.com>
2024-03-25 03:10:09 +0200

    Modified tests

--- tests/db.js
+++ tests/db.js
@@ -1,37 +1,22 @@
-// tests/db-handler.js
-
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
 
 const mongod = new MongoMemoryServer();
 
-/**
- * Connect to the in-memory database.
- */
-
 module.exports.connect = async () => {
   if (!mongod.isRunning) {
-    // Start the server if it's not already running
     await mongod.start();
   }
-
-  const uri = mongod.getUri(); // Use getUri() directly without awaiting
-
+  const uri = mongod.getUri();
   await mongoose.connect(uri);
 };
 
-/**
- * Drop database, close the connection and stop mongod.
- */
 module.exports.closeDatabase = async () => {
   await mongoose.connection.dropDatabase();
   await mongoose.connection.close();
   await mongod.stop();
 };
 
-/**
- * Remove all the data for all db collections.
- */
 module.exports.clearDatabase = async () => {
   const collections = mongoose.connection.collections;
 
