f4be482569dc2696fee14197d2b5a553467ec884
a0uda <abdullah.oudaa@gmail.com>
2024-04-11 04:45:48 +0200

    Update mongoose.js

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
-//const url = String(process.env.MONGODB_URL);
-const url = String(secrets.MONGODB_URL);
+const url = String(process.env.MONGODB_URL);
+//const url = String(secrets.MONGODB_URL);
 export async function connect_to_db() {
-  console.log(secrets.MONGODB_URL);
+  //console.log(secrets.MONGODB_URL);
   await mongoose.connect(url);
   console.log("Connected to db");
 }
