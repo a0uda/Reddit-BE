46ead59955a0921500eea63b3d8ab905ca238c64
a0uda <abdullah.oudaa@gmail.com>
2024-04-11 04:40:56 +0200

    secrets mongooz

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,9 +1,10 @@
 import mongoose from "mongoose";
-import dotenv from 'dotenv'
-dotenv.config()
-const url = String(process.env.MONGODB_URL);
+import dotenv from "dotenv";
+dotenv.config();
+//const url = String(process.env.MONGODB_URL);
+const url = String(secrets.MONGODB_URL);
 export async function connect_to_db() {
-    console.log(process.env.MONGODB_URL)
-    await mongoose.connect(url);
-    console.log("Connected to db");
+  console.log(secrets.MONGODB_URL);
+  await mongoose.connect(url);
+  console.log("Connected to db");
 }
