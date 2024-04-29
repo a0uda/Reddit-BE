703ed769c6f742543b6112efa461668f91caf9c5
a0uda <abdullah.oudaa@gmail.com>
2024-04-29 03:00:45 +0300

    cors part 3

--- src/index.js
+++ src/index.js
@@ -17,6 +17,9 @@ const app = express();
 
 app.use(express.json());
 
+app.use(cors());
+
+
 // const whitelist = ['http://localhost:5174', 'http://localhost:5173'];
 // const corsOptions = {
 //   origin: function (origin, callback) {
@@ -29,18 +32,7 @@ app.use(express.json());
 // };
 
 // app.use(cors(corsOptions));
-const whitelist = ["http://localhost:5174", "http://localhost:5173"];
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-};
 
-app.use(cors(corsOptions));
 
 //Connect to database
 console.log("port");
