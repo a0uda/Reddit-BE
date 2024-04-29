40592a19ca28fcb776f4ea0b007aae37897cce60
Malakyasser8 <malakyasser8@gmail.com>
2024-04-29 21:02:45 +0300

    Removed chats

--- src/controller/chatController.js
+++ src/controller/chatController.js
@@ -1,84 +1,84 @@
-import ChatModel from "../db/models/ChatModel.js"
-import MessageModel from "../db/models/MessageModel.js"
-import { User } from "../db/models/User.js";
+// import ChatModel from "../db/models/ChatModel.js"
+// import MessageModel from "../db/models/MessageModel.js"
+// import { User } from "../db/models/User.js";
 
-import { getReceiverSocketId, io } from "../socket/socket.js";
+// import { getReceiverSocketId, io } from "../socket/socket.js";
 
-// TODO: Error handling in all three functions is missing.
+// // TODO: Error handling in all three functions is missing.
 
-export const sendMessage = async (req, res) => {
-	try {
-		const { message } = req.body;
-		const { id: receiverId } = req.params;
-		const senderId = req.user._id;
+// export const sendMessage = async (req, res) => {
+// 	try {
+// 		const { message } = req.body;
+// 		const { id: receiverId } = req.params;
+// 		const senderId = req.user._id;
 
-		let chat = await ChatModel.findOne({
-			participants: { $all: [senderId, receiverId] },
-		});
+// 		let chat = await ChatModel.findOne({
+// 			participants: { $all: [senderId, receiverId] },
+// 		});
 
-		if (!chat) {
-			chat = await ChatModel.create({
-				participants: [senderId, receiverId],
-			});
-		}
+// 		if (!chat) {
+// 			chat = await ChatModel.create({
+// 				participants: [senderId, receiverId],
+// 			});
+// 		}
 
-		const newMessage = new MessageModel({
-			senderId,
-			receiverId,
-			message,
-		});
+// 		const newMessage = new MessageModel({
+// 			senderId,
+// 			receiverId,
+// 			message,
+// 		});
 
-		if (newMessage) {
-			ChatModel.messages.push(newMessage._id);
-		}
+// 		if (newMessage) {
+// 			ChatModel.messages.push(newMessage._id);
+// 		}
 
-		// this will run in parallel
-		await Promise.all([chat.save(), newMessage.save()]);
+// 		// this will run in parallel
+// 		await Promise.all([chat.save(), newMessage.save()]);
 
-		// SOCKET IO FUNCTIONALITY WILL GO HERE
-		const receiverSocketId = getReceiverSocketId(receiverId);
-		if (receiverSocketId) {
-			// io.to(<socket_id>).emit() used to send events to specific client
-			io.to(receiverSocketId).emit("newMessage", newMessage);
-		}
+// 		// SOCKET IO FUNCTIONALITY WILL GO HERE
+// 		const receiverSocketId = getReceiverSocketId(receiverId);
+// 		if (receiverSocketId) {
+// 			// io.to(<socket_id>).emit() used to send events to specific client
+// 			io.to(receiverSocketId).emit("newMessage", newMessage);
+// 		}
 
-		return res.status(201).json(newMessage);
-	} catch (error) {
-		console.log("Error in sendMessage controller: ", error.message);
-		res.status(500).json({ error: "Internal server error" });
-	}
-};
+// 		return res.status(201).json(newMessage);
+// 	} catch (error) {
+// 		console.log("Error in sendMessage controller: ", error.message);
+// 		res.status(500).json({ error: "Internal server error" });
+// 	}
+// };
 
-export const getMessages = async (req, res) => {
-	try {
-		const { id: userToChatId } = req.params;
-		const senderId = req.user._id;
+// export const getMessages = async (req, res) => {
+// 	try {
+// 		const { id: userToChatId } = req.params;
+// 		const senderId = req.user._id;
 
-		const chat = await ChatModel.findOne({
-			participants: { $all: [senderId, userToChatId] },
-		}).populate("messages"); // NOT REFERENCE BUT ACTUAL MESSAGES
+// 		const chat = await ChatModel.findOne({
+// 			participants: { $all: [senderId, userToChatId] },
+// 		}).populate("messages"); // NOT REFERENCE BUT ACTUAL MESSAGES
 
-		if (!chat) return res.status(200).json([]);
+// 		if (!chat) return res.status(200).json([]);
 
-		const messages = chat.messages;
+// 		const messages = chat.messages;
 
-		res.status(200).json(messages);
-	} catch (error) {
-		console.log("Error in getMessages controller: ", error.message);
-		res.status(500).json({ error: "Internal server error" });
-	}
-};
+// 		res.status(200).json(messages);
+// 	} catch (error) {
+// 		console.log("Error in getMessages controller: ", error.message);
+// 		res.status(500).json({ error: "Internal server error" });
+// 	}
+// };
 
-// TODO: The filter must be re-written.
-export const getUsersForSidebar = async (req, res) => {
-	try {
-		const loggedInUserId = req.user._id;
+// // TODO: The filter must be re-written.
+// export const getUsersForSidebar = async (req, res) => {
+// 	try {
+// 		const loggedInUserId = req.user._id;
 
-		const filteredUsers = await User.find({ _id: { $ne: loggedInUserId } }).select("-password");
+// 		const filteredUsers = await User.find({ _id: { $ne: loggedInUserId } }).select("-password");
 
-		res.status(200).json(filteredUsers);
-	} catch (error) {
-		console.error("Error in getUsersForSidebar: ", error.message);
-		res.status(500).json({ error: "Internal server error" });
-	}
-};
+// 		res.status(200).json(filteredUsers);
+// 	} catch (error) {
+// 		console.error("Error in getUsersForSidebar: ", error.message);
+// 		res.status(500).json({ error: "Internal server error" });
+// 	}
+// };
--- src/routers/chatRouter.js
+++ src/routers/chatRouter.js
@@ -1,12 +1,12 @@
-import express from "express";
-import { getMessages, sendMessage, getUsersForSidebar } from "../controller/chatController.js";
-import protectRoute from "../middleware/protectRoutes.js";
+// import express from "express";
+// import { getMessages, sendMessage, getUsersForSidebar } from "../controller/chatController.js";
+// import protectRoute from "../middleware/protectRoutes.js";
 
-const chatRouter = express.Router();
+// const chatRouter = express.Router();
 
-chatRouter.get("/chats/:id", protectRoute, getMessages);
-chatRouter.post("/chats/send/:id", protectRoute, sendMessage);
+// chatRouter.get("/chats/:id", protectRoute, getMessages);
+// chatRouter.post("/chats/send/:id", protectRoute, sendMessage);
 
-chatRouter.get("/chats/", protectRoute, getUsersForSidebar);
+// chatRouter.get("/chats/", protectRoute, getUsersForSidebar);
 
-export default chatRouter;
+// export default chatRouter;
