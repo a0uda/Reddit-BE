1b9a6a894470d26ccd372c391c6cd00865353e58
Malakyasser8 <malakyasser8@gmail.com>
2024-05-02 17:06:24 +0300

    Removed fuzzy searching func

--- src/db/models/User.js
+++ src/db/models/User.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import validator from "validator";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import mongooseFuzzySearching from "mongoose-fuzzy-searching";
+
 const userSchema = new mongoose.Schema({
   created_at: {
     type: Date,
