641da7ee7228997c97a5e0e282ca7737bfe86e37
Malakyasser8 <malakyasser8@gmail.com>
2024-05-08 16:19:43 +0300

    Added unit tests

--- __test__/notifications.test.js
+++ __test__/notifications.test.js
@@ -1,6 +1,9 @@
 import {
   pushNotification,
   getNotifications,
+  markAsRead,
+  hideNotification,
+  getUnreadNotificationsCount,
 } from "../src/controller/notifications";
 import { User } from "../src/db/models/User";
 import { Notification } from "../src/db/models/Notification";
@@ -104,6 +107,38 @@ describe("pushNotification", () => {
       sending_user_username,
       post,
       null,
+      "comments"
+    );
+    expect(result.success).toBe(false);
+    expect(result.error).toBe("User has this community muted");
+  });
+
+  it("should return error if comment is in a muted community", async () => {
+    const user = {
+      _id: "user_id",
+      username: "test_user",
+      safety_and_privacy_settings: {
+        muted_communities: [{ id: "muted_community_id" }],
+        blocked_users: [],
+      },
+      notifications_settings: {
+        comments: true,
+      },
+    };
+
+    const sending_user_username = "sending_user";
+    const comment = {
+      _id: "post_id",
+      comment_in_community_flag: true,
+      community_id: "muted_community_id",
+      community_name: "muted_community",
+    };
+
+    const result = await pushNotification(
+      user,
+      sending_user_username,
+      null,
+      comment,
       "comments"
     );
     expect(result.success).toBe(false);
@@ -249,7 +284,7 @@ describe("getNotifications", () => {
       headers: {},
     };
     const result = await getNotifications(request);
-    
+
     expect(result.success).toBe(false);
     expect(result.error.status).toEqual(401);
     expect(result.error.message).toEqual("Access Denied");
@@ -322,9 +357,11 @@ describe("getNotifications", () => {
   //   User.findById.mockResolvedValueOnce(mockUser);
   //   jwt.verify.mockReturnValue({ _id: mockUser._id });
 
-  //   const mockFindNotif = jest.fn().mockResolvedValue(mockNotifications);
-  //   const mockSort = jest.fn(() => ({ exec: mockFindNotif }));
-  //   Notification.find.mockReturnValueOnce({ sort: mockSort });
+  //   const mockQuery = {
+  //     sort: jest.fn().mockReturnThis(),
+  //     exec: jest.fn().mockResolvedValue(mockNotifications),
+  //   };
+  //   Notification.find = jest.fn().mockReturnValue(mockQuery);
 
   //   const mockFindCom = jest.fn().mockResolvedValue(mockCommunities);
   //   Community.find.mockReturnValueOnce({ exec: mockFindCom });
@@ -355,4 +392,450 @@ describe("getNotifications", () => {
   //   const result = await getNotifications(request);
   //   expect(result).toEqual(expectedResult);
   // });
+
+  // it("should return an error response if there is an internal server error", async () => {
+  //   const request = {
+  //     headers: {
+  //       authorization: "Bearer valid_token",
+  //     },
+  //   };
+  //   const mockUser = {
+  //     _id: "user_id",
+  //     token: ["valid_token"],
+  //     profile_picture: "user_profile_picture",
+  //     generateAuthToken: jest.fn(),
+  //     save: jest.fn(),
+  //   };
+  //   User.findById.mockResolvedValueOnce(mockUser);
+  //   jwt.verify.mockReturnValueOnce({ _id: mockUser._id });
+
+  //   const mockQuery = {
+  //     sort: jest.fn().mockReturnThis(),
+  //     exec: jest.fn().mockResolvedValue(null),
+  //   };
+  //   Notification.find = jest.fn().mockReturnValue(mockQuery);
+
+  //   const result = await getNotifications(request);
+  //   expect(result).toEqual({
+  //     success: false,
+  //     error: {
+  //       status: 500,
+  //       message: "Internal Server error",
+  //     },
+  //   });
+  // });
+
+});
+
+describe("markAsRead", () => {
+  it("should return error if token is missing", async () => {
+    const request = {
+      headers: {},
+    };
+    const result = await markAsRead(request);
+
+    expect(result.success).toBe(false);
+    expect(result.error.status).toEqual(401);
+    expect(result.error.message).toEqual("Access Denied");
+  });
+
+  it("should mark all notifications as read if markAllFlag is true", async () => {
+    const request = {
+      headers: {
+        authorization: "Bearer valid_token",
+      },
+    };
+
+    const mockUser = {
+      _id: "user_id",
+      token: ["valid_token"],
+      profile_picture: "user_profile_picture",
+      generateAuthToken: jest.fn(),
+      save: jest.fn(),
+    };
+    User.findById.mockResolvedValueOnce(mockUser);
+    jwt.verify.mockReturnValue({ _id: mockUser._id });
+
+    // Mock Notification.find to return mock notifications
+    const mockNotifications = [
+      {
+        _id: "notification_id_1",
+        user_id: "user_id",
+        unread_flag: true,
+        save: jest.fn(),
+      },
+      {
+        _id: "notification_id_2",
+        user_id: "user_id",
+        unread_flag: true,
+        save: jest.fn(),
+      },
+    ];
+    const mockExec = jest.fn().mockResolvedValue(mockNotifications);
+    const mockFind = jest.fn().mockReturnValueOnce({ exec: mockExec });
+    Notification.find = mockFind;
+
+    const result = await markAsRead(request, true);
+
+    // Assert response
+    expect(result).toEqual({
+      success: true,
+      message: "All Notifications are read successfully",
+    });
+  });
+
+  it("should mark a single notification as read if markAllFlag is false and valid id is provided", async () => {
+    const request = {
+      headers: {
+        authorization: "Bearer valid_token",
+      },
+      body: {
+        id: "notification_id",
+      },
+    };
+
+    // Mock the user and authentication
+    const mockUser = {
+      _id: "user_id",
+      token: ["valid_token"],
+      profile_picture: "user_profile_picture",
+      generateAuthToken: jest.fn(),
+      save: jest.fn(),
+    };
+    User.findById.mockResolvedValueOnce(mockUser);
+    jwt.verify.mockReturnValueOnce({ _id: mockUser._id });
+
+    // Mock the notification to be found
+    const mockNotification = {
+      _id: "notification_id",
+      user_id: "user_id",
+      unread_flag: true,
+      save: jest.fn(),
+    };
+    Notification.findById.mockResolvedValueOnce(mockNotification);
+
+    const result = await markAsRead(request, false);
+
+    expect(result).toEqual({
+      success: true,
+      message: "Notification read successfully",
+    });
+  });
+
+  it("should return an error response if notification id is not provided", async () => {
+    const request = {
+      headers: {
+        authorization: "Bearer valid_token",
+      },
+      body: {}, // No id property provided
+    };
+
+    // Mock the user and authentication
+    const mockUser = {
+      _id: "user_id",
+      token: ["valid_token"],
+      profile_picture: "user_profile_picture",
+      generateAuthToken: jest.fn(),
+      save: jest.fn(),
+    };
+    User.findById.mockResolvedValueOnce(mockUser);
+    jwt.verify.mockReturnValueOnce({ _id: mockUser._id });
+
+    const result = await markAsRead(request, false);
+
+    expect(result).toEqual({
+      success: false,
+      error: {
+        status: 400,
+        message: "Notification id is required",
+      },
+    });
+  });
+
+  it("should return an error response if notification id is not found", async () => {
+    const request = {
+      headers: {
+        authorization: "Bearer valid_token",
+      },
+      body: {
+        id: "non_existent_notification_id",
+      },
+    };
+
+    const mockUser = {
+      _id: "user_id",
+      token: ["valid_token"],
+      profile_picture: "user_profile_picture",
+      generateAuthToken: jest.fn(),
+      save: jest.fn(),
+    };
+    User.findById.mockResolvedValueOnce(mockUser);
+    jwt.verify.mockReturnValueOnce({ _id: mockUser._id });
+
+    Notification.findById.mockResolvedValueOnce(null);
+
+    const result = await markAsRead(request, false);
+    expect(result).toEqual({
+      success: false,
+      error: {
+        status: 400,
+        message: "Notification is not found",
+      },
+    });
+  });
+
+  it("should return an error response if there is an internal server error", async () => {
+    const request = {
+      headers: {
+        authorization: "Bearer valid_token",
+      },
+      body: {
+        id: "notification_id",
+      },
+    };
+    const mockUser = {
+      _id: "user_id",
+      token: ["valid_token"],
+      profile_picture: "user_profile_picture",
+      generateAuthToken: jest.fn(),
+      save: jest.fn(),
+    };
+    User.findById.mockResolvedValueOnce(mockUser);
+    jwt.verify.mockReturnValueOnce({ _id: mockUser._id });
+
+    Notification.findById.mockRejectedValueOnce(new Error("Database error"));
+
+    const result = await markAsRead(request);
+    expect(result).toEqual({
+      success: false,
+      error: {
+        status: 500,
+        message: "Internal Server error",
+      },
+    });
+  });
+});
+
+describe("hideNotification", () => {
+  it("should return error if token is missing", async () => {
+    const request = {
+      headers: {},
+    };
+    const result = await hideNotification(request);
+
+    expect(result.success).toBe(false);
+    expect(result.error.status).toEqual(401);
+    expect(result.error.message).toEqual("Access Denied");
+  });
+
+  it("should hide a notification if valid id is provided", async () => {
+    const request = {
+      headers: {
+        authorization: "Bearer valid_token",
+      },
+      body: {
+        id: "notification_id",
+      },
+    };
+
+    const mockUser = {
+      _id: "user_id",
+      token: ["valid_token"],
+      profile_picture: "user_profile_picture",
+      generateAuthToken: jest.fn(),
+      save: jest.fn(),
+    };
+    User.findById.mockResolvedValueOnce(mockUser);
+    jwt.verify.mockReturnValueOnce({ _id: mockUser._id });
+
+    // Mock Notification.findById to return a mock notification
+    const mockNotification = {
+      _id: "notification_id",
+      hidden_flag: false,
+      save: jest.fn(),
+    };
+    Notification.findById.mockResolvedValueOnce(mockNotification);
+
+    const result = await hideNotification(request);
+
+    expect(result).toEqual({
+      success: true,
+      message: "Notification hidden successfully",
+    });
+  });
+
+  it("should return an error response if notification id is not provided", async () => {
+    const request = {
+      headers: {
+        authorization: "Bearer valid_token",
+      },
+      body: {}, // No id property provided
+    };
+
+    const mockUser = {
+      _id: "user_id",
+      token: ["valid_token"],
+      profile_picture: "user_profile_picture",
+      generateAuthToken: jest.fn(),
+      save: jest.fn(),
+    };
+    User.findById.mockResolvedValueOnce(mockUser);
+    jwt.verify.mockReturnValueOnce({ _id: mockUser._id });
+
+    const result = await hideNotification(request);
+
+    // Assert response
+    expect(result).toEqual({
+      success: false,
+      error: {
+        status: 400,
+        message: "Notification id is required",
+      },
+    });
+  });
+
+  it("should return an error response if notification id is not found", async () => {
+    const request = {
+      headers: {
+        authorization: "Bearer valid_token",
+      },
+      body: {
+        id: "non_existent_notification_id",
+      },
+    };
+
+    const mockUser = {
+      _id: "user_id",
+      token: ["valid_token"],
+      profile_picture: "user_profile_picture",
+      generateAuthToken: jest.fn(),
+      save: jest.fn(),
+    };
+    User.findById.mockResolvedValueOnce(mockUser);
+    jwt.verify.mockReturnValueOnce({ _id: mockUser._id });
+
+    Notification.findById.mockResolvedValueOnce(null);
+
+    const result = await hideNotification(request);
+
+    expect(result).toEqual({
+      success: false,
+      error: {
+        status: 400,
+        message: "Notification is not found",
+      },
+    });
+  });
+
+  it("should return an error response if there is an internal server error", async () => {
+    const request = {
+      headers: {
+        authorization: "Bearer valid_token",
+      },
+      body: {
+        id: "notification_id",
+      },
+    };
+    const mockUser = {
+      _id: "user_id",
+      token: ["valid_token"],
+      profile_picture: "user_profile_picture",
+      generateAuthToken: jest.fn(),
+      save: jest.fn(),
+    };
+    User.findById.mockResolvedValueOnce(mockUser);
+    jwt.verify.mockReturnValueOnce({ _id: mockUser._id });
+
+    Notification.findById.mockRejectedValueOnce(new Error("Database error"));
+
+    const result = await hideNotification(request);
+    expect(result).toEqual({
+      success: false,
+      error: {
+        status: 500,
+        message: "Internal Server error",
+      },
+    });
+  });
+});
+
+describe("getUnreadNotificationsCount", () => {
+  it("should return the count of unread notifications if user is authenticated", async () => {
+    const request = {
+      headers: {
+        authorization: "Bearer valid_token",
+      },
+    };
+
+    const mockUser = {
+      _id: "user_id",
+      token: ["valid_token"],
+      profile_picture: "user_profile_picture",
+      generateAuthToken: jest.fn(),
+      save: jest.fn(),
+    };
+
+    User.findById.mockResolvedValueOnce(mockUser);
+    jwt.verify.mockReturnValueOnce({ _id: mockUser._id });
+
+    const mockNotifications = [
+      { _id: "notification_id_1", user_id: "user_id", unread_flag: true },
+      { _id: "notification_id_2", user_id: "user_id", unread_flag: true },
+    ];
+    const mockExec = jest.fn().mockResolvedValue(mockNotifications);
+    const mockFind = jest.fn().mockReturnValueOnce({ exec: mockExec });
+    Notification.find = mockFind;
+
+    const result = await getUnreadNotificationsCount(request);
+
+    // Assert response
+    expect(result).toEqual({
+      success: true,
+      message: "Notifications retrieved successfully",
+      count: mockNotifications.length,
+    });
+  });
+
+  it("should return error if token is missing", async () => {
+    const request = {
+      headers: {},
+    };
+    const result = await getUnreadNotificationsCount(request);
+
+    expect(result.success).toBe(false);
+    expect(result.error.status).toEqual(401);
+    expect(result.error.message).toEqual("Access Denied");
+  });
+
+  it("should return an error response if there is an internal server error", async () => {
+    const request = {
+      headers: {
+        authorization: "Bearer valid_token",
+      },
+    };
+
+    const mockUser = {
+      _id: "user_id",
+      token: ["valid_token"],
+      profile_picture: "user_profile_picture",
+      generateAuthToken: jest.fn(),
+      save: jest.fn(),
+    };
+    User.findById.mockResolvedValueOnce(mockUser);
+    jwt.verify.mockReturnValueOnce({ _id: mockUser._id });
+
+    const mockFind = jest.fn().mockReturnValueOnce(new Error("Database error"));
+    Notification.find = mockFind;
+
+    const result = await getUnreadNotificationsCount(request);
+
+    // Assert response
+    expect(result).toEqual({
+      success: false,
+      error: {
+        status: 500,
+        message: "Internal Server error",
+      },
+    });
+  });
 });
