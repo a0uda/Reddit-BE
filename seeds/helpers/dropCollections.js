e380508f028355134781ccc3962d25c2dc05bf74
Malakyasser8 <malakyasser8@gmail.com>
2024-05-10 13:38:34 +0300

    Fixed a flag

--- seeds/helpers/dropCollections.js
+++ seeds/helpers/dropCollections.js
@@ -1,30 +1,32 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const url = "mongodb+srv://ahmedabdelgawad011:BackendReddit@cluster0.workift.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+const url =
+  "mongodb+srv://ahmedabdelgawad011:BackendReddit@cluster0.workift.mongodb.net/?retryWrites=true&w=majority";
 
 export async function dropCollections() {
-    try {
-        console.log("Conneting to db")
-        await mongoose.connect(url);
-        console.log("Connected to db")
+  try {
+    console.log("Conneting to db");
+    await mongoose.connect(url);
+    console.log("Connected to db");
 
-        console.log("The collections are")
-        const collections = await mongoose.connection.db.listCollections().toArray()
-        console.log(collections);
+    console.log("The collections are");
+    const collections = await mongoose.connection.db
+      .listCollections()
+      .toArray();
+    console.log(collections);
 
-        for (const { name } of collections) {
-            if (name === 'users' || name === 'tokens') {
-                console.log(`Skipping ${name} collection`);
-                continue;
-            }
+    for (const { name } of collections) {
+      if (name === "users" || name === "tokens") {
+        console.log(`Skipping ${name} collection`);
+        continue;
+      }
 
-            console.log(`Dropping collection: ${name}`);
-            await mongoose.connection.collection(name).drop();
-        }
-
-    } catch (error) {
-        console.error('Error dropping collections:', error);
-    } finally {
-        mongoose.connection.close();
+      console.log(`Dropping collection: ${name}`);
+      await mongoose.connection.collection(name).drop();
     }
+  } catch (error) {
+    console.error("Error dropping collections:", error);
+  } finally {
+    mongoose.connection.close();
+  }
 }
